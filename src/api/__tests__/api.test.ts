5b3654008c1a2b48baf9d6259ed46a6c238e9398
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-01-14 01:49:02 +0100

    API toolkit

---
+++ src/api/__tests__/api.test.ts
@@ -0,0 +1,41 @@
+import { BASE_URL, find_url, try_get_resource } from "../api";
+
+import BOARDS from "./boards.json";
+import Board from "../board";
+import redaxios from "redaxios";
+
+describe("find_url", () => {
+  describe("/boards", () => {
+    test("/", () => {
+      expect(find_url({ board: "" })).toStrictEqual(
+        new URL("/boards/", BASE_URL),
+      );
+    });
+
+    test("/test", () => {
+      expect(find_url({ board: "test" })).toStrictEqual(
+        new URL("/boards/test", BASE_URL),
+      );
+    });
+
+    describe("/test/threads", () => {
+      test("/", () => {
+        expect(find_url({ board: "test", thread: "" })).toStrictEqual(
+          new URL("/boards/test/threads", BASE_URL),
+        );
+      });
+
+      test("/123", () => {
+        expect(find_url({ board: "test", thread: "123" })).toStrictEqual(
+          new URL("/boards/test/threads/123", BASE_URL),
+        );
+      });
+    });
+  });
+
+  test("fails /boards/?/threads/123", () => {
+    expect(() => {
+      find_url({ thread: "123" });
+    }).toThrow("No match");
+  });
+});
---
+++ src/api/api.ts
@@ -0,0 +1,84 @@
+import "format-unicorn";
+
+import axios from "redaxios";
+
+export const BASE_URL = "https://chan-api-ri.herokuapp.com";
+
+class UrlSegment {
+  private segment;
+  private static param_exp = new RegExp(/{([^{]*)}/g);
+  public isParameterized;
+  constructor(segment: string) {
+    this.segment = segment;
+    this.isParameterized = UrlSegment.param_exp.test(segment);
+  }
+
+  public static segments(relative_url: string): UrlSegment[] {
+    return relative_url.split("/").map((segment) => new UrlSegment(segment));
+  }
+
+  public toString(): string {
+    let str = this.segment;
+
+    if (this.isParameterized) {
+      str = str.substring(1, str.length - 1);
+    }
+
+    return str;
+  }
+}
+
+class Endpoint {
+  private relative_url;
+  private segments;
+
+  constructor(relative_url: string) {
+    this.relative_url = relative_url;
+    this.segments = UrlSegment.segments(relative_url);
+  }
+
+  public get parameterized_segments(): UrlSegment[] {
+    return this.segments.filter((segment) => segment.isParameterized);
+  }
+
+  public matching(params: APIParams): boolean {
+    const segments = this.parameterized_segments.map((segment) =>
+      segment.toString(),
+    );
+
+    return segments.every((segment) => segment in params);
+  }
+
+  public format(params: APIParams): string {
+    return this.relative_url.formatUnicorn(params);
+  }
+}
+
+const ENDPOINTS = ["/boards/{board}", "/boards/{board}/threads/{thread}"].map(
+  (endpoint) => new Endpoint(endpoint),
+);
+
+export interface APIParams {
+  board?: string;
+  thread?: string;
+}
+
+/**
+ * @throws {Error}
+ */
+export function find_url(params: APIParams): URL {
+  const endpoint = ENDPOINTS.find((_endpoint) => _endpoint.matching(params));
+
+  if (!endpoint) {
+    throw new Error("No match");
+  }
+
+  return new URL(endpoint.format(params), BASE_URL);
+}
+
+export async function try_get_resource<T>(params: APIParams): Promise<T> {
+  const url = find_url(params);
+  const response = await axios.get<T>(url.toString());
+
+  return response.data;
+}
