4e65d4512318dcc53572f937d10c6c959b7484de
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-01-14 01:50:12 +0100

    List and filter boards
    Can't click on them yet

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,98 @@
-<script lang="ts">
-  import Board from "../api/board";
+<style lang="sass">
+  .boards
+    display: grid
+    column-gap: .5em
+    row-gap: .5em
+    width: calc(100% - 1em)
+    max-width: 1200px
+    grid-template-columns: repeat(auto-fill, minmax(320px, 1fr))
 
-  const promise = Board.fetchAll();
+  .center-h
+    width: 100%
+    display: flex
+    justify-content: center
+    overflow-y: auto
+    
+  .center-v
+    height: 100%
+    display: flex
+    flex-direction: column-reverse
+    justify-content: space-between
+
+
+  input
+    font-size: 1.5rem
+    padding: .5em
+    max-width: 800px
+    width: 100%
+    box-sizing: border-box
+    align-self: center
+    margin: .5em
+    border: none
+    background-color: #EAE4E9
+    border-radius: .3em
+
+  .wrapper
+    height: 100vh
+
+  .bottom
+    display: flex
+    justify-content: center
+
+</style>
+
+<script lang="ts">
+  import BoardSelect from "../ui/BoardSelect.svelte";
+  import Board from "../api/board";
+  import Fuse from "fuse.js";
+  import debounce from "debounce";
+
+  const search_opts: Fuse.IFuseOptions<Board> = {
+    keys: ["name"],
+  };
+  let search = "";
+  let fuse: Fuse<Board>;
+  let promise = Board.fetchAll().then((bs) => {
+    fuse = new Fuse(bs, { keys: ["name"] });
+    return bs;
+  });
+  $: filtered = promise.then((bs) => {
+    if (!search.trim()) {
+      return bs;
+    }
+    return fuse.search(search).map((r) => r.item);
+  });
+
+  function _search(e: Event) {
+    if (!fuse) {
+      return;
+    }
+    search = (e.target as HTMLInputElement).value;
+  }
+
+  const handleSearch = debounce(_search, 200);
 </script>
 
-{#await promise}
+<div class="wrapper">
+  <div class="center-v">
+    <div class="bottom">
+      <input type="text" on:input={handleSearch} placeholder="Search..." />
+    </div>
+    <div class="center-h">
+      {#await filtered}
+        <p>Loading</p>
+      {:then boards}
+        <div class="boards">
+          {#each boards as board}
+            <BoardSelect board={board} />
+          {/each}
+        </div>
+      {/await}
+    </div>
+  </div>
+</div>
+
+<!-- {#await promise}
   <span>Loading</span>
 {:then boards}
   {#each boards as board}
@@ -13,3 +101,4 @@
 {:catch error}
   <span>{error.message}</span>
 {/await}
+ -->
---
+++ src/ui/BoardSelect.svelte
@@ -0,0 +1,48 @@
+<style lang="sass">
+  .board-select
+    cursor: pointer
+    display: flex
+    background-color: #DFE7FD
+    font-size: 2rem
+    padding: .3em
+    border-radius: 1rem
+    transition: background-color 100ms
+    color: darken(#DFE7FD, 80%)
+    &:hover
+      background-color: darken(#DFE7FD, 5%)
+    &:hover .shorthand
+      background-color: darken(#CDDAFD, 5%)
+    
+  .shorthand
+    display: flex
+    background-color: #CDDAFD
+    width: 2em
+    height: 2em
+    justify-content: center
+    align-items: center
+    border-radius: 1rem
+    transition: background-color 100ms
+    font-weight: bold
+    text-transform: uppercase
+
+  .body
+    display: flex
+    justify-content: center
+    align-items: center
+    margin-left: .3em
+
+</style>
+
+<script lang="ts">
+  import Board from "api/board";
+  export let board: Board;
+</script>
+
+<div class="board-select">
+  <div class="shorthand">
+    {board.shorthand}
+  </div>
+  <div class="body">
+    {board.name}
+  </div>
+</div>
