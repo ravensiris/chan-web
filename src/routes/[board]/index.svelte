04eb36558c673374fea3042b895adac17193320e
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-01-16 23:17:29 +0100

    Fetching threads

---
+++ src/routes/[board]/index.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import {params} from '@roxi/routify';
+    import Thread from '../../api/thread';
+
+    let promise = Thread.fetchAll($params.board);
+    console.log($params.board);
+</script>
+
+{#await promise}
+<p>Loading</p>
+{:then threads}
+   {#each threads as thread}
+        <h1>{thread.op.title}</h1>
+        <pre>{thread.op.body}</pre>
+   {/each} 
+{/await}
