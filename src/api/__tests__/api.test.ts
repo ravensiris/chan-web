95911eb636fbd5c00fe94e16614b7f9e4e7566fd
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-01-16 23:14:20 +0100

    Fix tests

--- src/api/__tests__/api.test.ts
+++ src/api/__tests__/api.test.ts
@@ -3,27 +3,27 @@ import { BASE_URL, find_url, try_get_resource } from "../api";
 describe("find_url", () => {
   describe("/boards", () => {
     test("/", () => {
-      expect(find_url({ board: "" })).toStrictEqual(
-        new URL("/boards/", BASE_URL),
+      expect(find_url({ board: "" }).toString()).toStrictEqual(
+        new URL("/boards/", BASE_URL).toString(),
       );
     });
 
     test("/test", () => {
-      expect(find_url({ board: "test" })).toStrictEqual(
-        new URL("/boards/test", BASE_URL),
+      expect(find_url({ board: "test" }).toString()).toStrictEqual(
+        new URL("/boards/test", BASE_URL).toString(),
       );
     });
 
     describe("/test/threads", () => {
       test("/", () => {
-        expect(find_url({ board: "test", thread: "" })).toStrictEqual(
-          new URL("/boards/test/threads", BASE_URL),
+        expect(find_url({ board: "test", thread: "" }).toString()).toStrictEqual(
+          new URL("/boards/test/threads/", BASE_URL).toString(),
         );
       });
 
       test("/123", () => {
-        expect(find_url({ board: "test", thread: "123" })).toStrictEqual(
-          new URL("/boards/test/threads/123", BASE_URL),
+        expect(find_url({ board: "test", thread: "123" }).toString()).toStrictEqual(
+          new URL("/boards/test/threads/123", BASE_URL).toString(),
         );
       });
     });
